4f2ff7f929f8f1a317856f5638f17c6c6b3db4cc
MendozaXD2 <69319807+MendozaXD2@users.noreply.github.com>
2021-01-15 14:00:15 -0500

    Add files via upload

---
+++ Nitro Cracker/Nitro Cracker/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Nitro_Cracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroComboBox1 = new MetroFramework.Controls.MetroComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Start = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroComboBox1
+            // 
+            this.metroComboBox1.FontSize = MetroFramework.MetroLinkSize.Tall;
+            this.metroComboBox1.FormattingEnabled = true;
+            this.metroComboBox1.ItemHeight = 29;
+            this.metroComboBox1.Items.AddRange(new object[] {
+            "Http/s",
+            "Socks4",
+            "Socks5"});
+            this.metroComboBox1.Location = new System.Drawing.Point(238, 12);
+            this.metroComboBox1.Name = "metroComboBox1";
+            this.metroComboBox1.Size = new System.Drawing.Size(163, 35);
+            this.metroComboBox1.Style = MetroFramework.MetroColorStyle.Black;
+            this.metroComboBox1.TabIndex = 0;
+            this.metroComboBox1.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(-1, -1);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(549, 246);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Location = new System.Drawing.Point(420, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(549, 246);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(159, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Proxies: ";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel2.Location = new System.Drawing.Point(12, 172);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(389, 86);
+            this.panel2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(146, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Bad: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(15, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Tested: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(15, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Proxies: ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(199, 53);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(215, 45);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(93, 57);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 23);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Threads: 0";
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(12, 114);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(163, 52);
+            this.Start.Style = MetroFramework.MetroColorStyle.White;
+            this.Start.TabIndex = 7;
+            this.Start.Text = "Start";
+            this.Start.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(238, 114);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(163, 52);
+            this.metroButton2.Style = MetroFramework.MetroColorStyle.White;
+            this.metroButton2.TabIndex = 8;
+            this.metroButton2.Text = "Stop";
+            this.metroButton2.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(981, 269);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.metroComboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Nitro Code Cracker | By Sheepy";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroComboBox metroComboBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label6;
+        private MetroFramework.Controls.MetroButton Start;
+        private MetroFramework.Controls.MetroButton metroButton2;
+    }
+}
+
---
+++ Nitro Cracker/Nitro Cracker/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using Leaf.xNet;
+
+namespace Nitro_Cracker
+{
+    public partial class Form1 : Form
+    {
+        public static List<string> Proxies = new List<string>();
+        public static bool FoundCode = false;
+        public static Random rnd = new Random();
+        public static int Checked_C = 0;
+        public static int Errors = 0;
+        public static ProxyType proxytype;
+        public static object _lock = new object();
+        public Form1()
+        {
+            //Checkcode("", );
+            InitializeComponent();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            if (metroComboBox1.SelectedIndex == 0)
+                proxytype = ProxyType.HTTP;
+
+            else if (metroComboBox1.SelectedIndex == 1)
+                proxytype = ProxyType.Socks4;
+
+            else if (metroComboBox1.SelectedIndex == 2)
+                proxytype = ProxyType.Socks5;
+
+            else
+            {
+                MessageBox.Show("Please select a proxy type");
+                return;
+            }
+
+            int threadcount = trackBar1.Value;
+            new Thread(delegate ()
+            {
+                while (!FoundCode)
+                {
+                    List<Thread> threads = new List<Thread>();
+
+                    for (int l = 0; l < threadcount; l++)//SETTINGS
+                    {
+
+                        Thread t = new Thread(delegate ()
+                        {
+                            while (!FoundCode)
+                            {
+                                string code = GetCode();
+                                bool ok = false;
+                                var watch = System.Diagnostics.Stopwatch.StartNew();
+                                while (!ok)
+                                {
+                                    UpdateValues();
+
+
+                                    int linecount = 0;
+                                    richTextBox1.Invoke(new MethodInvoker(delegate () { linecount = richTextBox1.Lines.Count(); }));
+
+                                    if (linecount > 500)
+                                    {
+
+                                        richTextBox1.Invoke(new MethodInvoker(delegate () { richTextBox1.Lines.ToList().Clear(); }));
+                                    }
+
+                                    if (Proxies.Count() < 30)
+                                    {
+                                        Proxies = Load(@"./Proxies.txt");
+
+                                    }
+
+                                    try
+                                    {
+                                        string proxyaddy;
+                                        lock (_lock)
+                                        {
+                                            proxyaddy = Proxies.ElementAt(rnd.Next(0, Proxies.Count() - 1));
+                                        }
+
+                                        ok = Checkcode(code, proxyaddy);
+                                    }
+                                    catch (Exception)
+                                    {
+                                        //outToLog(e.Message);
+                                    }
+
+                                    UpdateValues();
+
+
+
+                                }
+                                Checked_C++;
+                                watch.Stop();
+                                var elapsedMs = watch.ElapsedMilliseconds;
+
+                                if (Proxies.Count() < 30)
+                                {
+                                    Proxies = Load(@"./Proxies.txt");
+                                }
+
+                                try
+                                {
+                                    //Globals.Last_50_Checktimes.Add(elapsedMs);
+
+                                    //Globals.CPM = (int)(Config.Threads * 60000 / Globals.Last_50_Checktimes.Average());
+                                }
+                                catch (Exception)
+                                {
+                                    //outToLog(e.Message);
+                                }
+                                UpdateValues();
+                            }
+                        });
+                        t.Start();
+                        threads.Add(t);
+                        Thread.Sleep(100);
+                    }
+                    foreach (Thread thread in threads.ToList())
+                    {
+
+                        thread.Join();
+                        threads.Remove(thread);
+                    }
+
+                }
+            }).Start();
+        }
+        public static List<string> Load(string dir)
+        {
+            List<string> toLoad = new List<string>();
+
+            foreach (string line in File.ReadLines(dir))
+            {
+                toLoad.Add(line.Replace("\n", ""));
+
+            }
+            return toLoad.Distinct().ToList();
+        }
+        private void UpdateValues()
+        {
+            label3.Invoke(new MethodInvoker(delegate () { label3.Text = "Tested: " + Checked_C; label4.Text = "Errors: " + Errors; }));
+        }
+        public static string GetCode()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxyzZ0123456789";
+            return new string(Enumerable.Repeat(chars, 24).Select(s => s[rnd.Next(s.Length)]).ToArray());
+        }
+        public bool Checkcode(string code, string proxyaddy)
+        {
+
+            try
+            {
+                using (var request = new HttpRequest())
+                {
+                    if (proxytype == ProxyType.Socks5)
+                    {
+                        request.Proxy = Socks5ProxyClient.Parse(proxyaddy);
+                    }
+                    else if (proxytype == ProxyType.Socks4)
+                    {
+                        request.Proxy = Socks4ProxyClient.Parse(proxyaddy);
+                    }
+                    else
+                    {
+                        request.Proxy = HttpProxyClient.Parse(proxyaddy);
+                    }
+
+                    string text  = request.Get("https://discordapp.com/api/v6/entitlements/gift-codes/" + code + "?with_application=true&with_subscription_plan=true").ToString();
+                    FoundCode = true;
+                    outToLog("Found Code --> " + code);
+                    outToLog("Found Code --> " + code);
+                    outToLog("Found Code --> " + code);
+                    outToLog("Found Code --> " + code);
+                    outToLog("Found Code --> " + code);
+                    outToLog("Found Code --> " + code);
+                    outToLog("Found Code --> " + code);
+                    outToLog("Found Code --> " + code);
+                    outToLog("Found Code --> " + code);
+                    outToLog("Found Code --> " + code);
+                    outToLog("Found Code --> " + code);
+                    outToLog("Found Code --> " + code);
+                    MessageBox.Show("You found a nitro code : " + code);
+
+                }
+                return true;
+            }
+            catch (HttpException ex)
+            {
+                if ( (int)ex.HttpStatusCode == 404)
+                {
+                    outToLog("Bad code " + code);
+
+                    return true;
+                    
+                }
+                else
+                {
+                    //outToLog("removed proxy " + proxyaddy);
+                    
+                    Errors++;
+                    return false;
+                }
+            }
+            
+        }
+
+        public void outToLog(string output)
+        {
+            richTextBox1.Invoke(new MethodInvoker(delegate () {
+                richTextBox1.AppendText("\r\n" + output);
+                richTextBox1.ScrollToCaret();
+            }));
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label6.Text = "Threads: " + trackBar1.Value;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
